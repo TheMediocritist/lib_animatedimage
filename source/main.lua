f2690ec17585e530e352d66caf93628b716ee355
TheMediocritist <79881777+TheMediocritist@users.noreply.github.com>
2022-06-07 13:05:59 +1000

    Update main.lua

--- source/main.lua
+++ source/main.lua
@@ -1,11 +1,10 @@
 import "CoreLibs/graphics"
-import "CoreLibs/sprites"
 import "animatedimage"
 
 local graphics <const> = playdate.graphics
 local geometry <const> = playdate.geometry
 
--- create animations using animatedimage library
+-- create animations using AnimatedImage library
 local animation = {}
 animation.walkS = AnimatedImage.new("spritesheet_walking", {sequence = {1, 2, 3, 4}, delay = 100, loop = true})
 animation.walkN = AnimatedImage.new("spritesheet_walking", {sequence = {5, 6, 7, 8}, delay = 100, loop = true})
@@ -15,7 +14,7 @@ animation.walkNW = AnimatedImage.new("spritesheet_walking", {sequence = {17, 18,
 animation.walkNE = AnimatedImage.new("spritesheet_walking", {sequence = {21, 22, 23, 24}, delay = 100, loop = true})
 animation.walkSW = AnimatedImage.new("spritesheet_walking", {sequence = {25, 26, 27, 28}, delay = 100, loop = true})
 animation.walkSE = AnimatedImage.new("spritesheet_walking", {sequence = {29, 30, 31, 32}, delay = 100, loop = true})
-animation.idle = AnimatedImage.new("spritesheet_walking", {sequence = {1}, delay = 100, loop = true})
+animation.idle = AnimatedImage.new("spritesheet_walking", {sequence = {1}, delay = 100, loop = false})
 
 -- setup input
 local input_vector = geometry.vector2D.new(0, 0)
